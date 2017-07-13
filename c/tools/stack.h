536e572e1e372ca0783943e428c42726b7e6e436
Saul Lawliet <october.sunbathe@gmail.com>
2017-07-13 20:50:06 +0800

    空字符串 现在 不入栈.

--- c/tools/stack.h
+++ c/tools/stack.h
@@ -12,9 +12,9 @@
 #define STACK_INIT_SIZE 64
 #endif
 
-#define PUTC(c, ch)      do { *(char*)stackPush(c, sizeof(char)) = (ch); } while(0)
+#define PUTC(c, ch)      do { *(char*)stackPush(c, sizeof(char)) = (ch); } while (0)
 #define PUTCP(c, p)      memcpy(stackPush(c, sizeof(char*)), p, sizeof(char*))
-#define PUTS(c, s, len)  memcpy(stackPush(c, len), s, len)
+#define PUTS(c, s, len)  do { if (len > 0) memcpy(stackPush(c, len), s, len); } while (0)
 #define PUTI(c, i)       memcpy(stackPush(c, sizeof(int)), i, sizeof(int))
 #define PUTIP(c, p)      memcpy(stackPush(c, sizeof(int*)), p, sizeof(int*))
 
