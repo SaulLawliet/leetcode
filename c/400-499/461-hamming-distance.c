64bb4b61b4e59af231cc164ff4a6a86d9933b8ba
Saul Lawliet <october.sunbathe@gmail.com>
2017-08-27 22:13:52 +0800

    AC 461

---
+++ c/400-499/461-hamming-distance.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017, Saul Lawliet <october dot sunbathe at gmail dot com>
+ * All rights reserved.
+ *
+ * 异或后计算1的个数
+ */
+
+#include "../test.h"
+
+int hammingDistance(int x, int y) {
+  int rtn = 0;
+  x ^= y;
+  while (x) {
+    rtn += x & 1;
+    x >>= 1;
+  }
+  return rtn;
+}
+
+void test(int expect, int x, int y) {
+  EXPECT_EQ_INT(expect, hammingDistance(x, y));
+}
+
+int main(void) {
+  test(2, 1, 4);
+
+  return testOutput();
+}
