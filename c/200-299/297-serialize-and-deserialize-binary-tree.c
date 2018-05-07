760ab5b4009812555c924ea0a25c08f0ebbe4305
Saul Lawliet <october.sunbathe@gmail.com>
2018-05-07 11:33:48 +0800

    AC 297

---
+++ c/200-299/297-serialize-and-deserialize-binary-tree.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018, Saul Lawliet <october dot sunbathe at gmail dot com>
+ * All rights reserved.
+ */
+
+#include "c/data-structures/binary-tree.h"
+#include "c/test.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/** Encodes a tree to a single string. */
+char *serialize(struct TreeNode *root) {
+  return treeToString(root);
+}
+
+/** Decodes your encoded data to tree. */
+struct TreeNode *deserialize(char *data) {
+  return treeParse(data);
+}
+
+// Your functions will be called as such:
+// char* data = serialize(root);
+// deserialize(data);
+
+void test(char *data) {
+  struct TreeNode *root = deserialize(data);
+  char *actual = serialize(root);
+
+  EXPECT_EQ_STRING_AND_FREE_ACTUAL(data, actual);
+
+  treeFree(root);
+}
+
+int main(void) {
+  test("[1,2,3,null,null,4,5]");
+
+  // https://leetcode.com/faq/#binary-tree
+  test("[]");
+  test("[1,2,3]");
+  test("[1,null,2,3]");
+  test("[5,4,7,3,null,2,null,-1,null,9]");
+
+  testOutput();
+}
