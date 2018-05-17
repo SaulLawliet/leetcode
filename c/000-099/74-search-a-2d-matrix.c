9511669c5f0defad8d2eba2c5f78c64884d0f1ef
Saul Lawliet <october.sunbathe@gmail.com>
2018-05-17 14:57:08 +0800

    AC 74

---
+++ c/000-099/74-search-a-2d-matrix.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018, Saul Lawliet <october dot sunbathe at gmail dot com>
+ * All rights reserved.
+ *
+ * 转换一下矩阵下标, 同样是二分查找
+ */
+
+#include <stdbool.h>
+#include "c/data-structures/array.h"
+#include "c/test.h"
+
+bool searchMatrix(int **matrix, int matrixRowSize, int matrixColSize, int target) {
+  int a = 0, b = matrixRowSize * matrixColSize - 1;
+  while (a <= b) {
+    int i = (a + b) / 2;
+    int v = matrix[i / matrixColSize][i % matrixColSize];
+    if (v > target) {
+      b = i - 1;
+    } else if (v < target) {
+      a = i + 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+void test(bool expect, const char *matrix, int target) {
+  arrayEntry *e = arrayParse(matrix, ARRAY_INT);
+
+  EXPECT_EQ_INT(expect, searchMatrix(arrayValue(e), arrayRow(e), arrayCol(e), target));
+
+  arrayFree(e);
+}
+
+int main(void) {
+  test(true,
+       "["
+       "[1,   3,  5,  7],"
+       "[10, 11, 16, 20],"
+       "[23, 30, 34, 50]"
+       "]",
+       3);
+  test(false,
+       "["
+       "[1,   3,  5,  7],"
+       "[10, 11, 16, 20],"
+       "[23, 30, 34, 50]"
+       "]",
+       13);
+
+  testOutput();
+}
